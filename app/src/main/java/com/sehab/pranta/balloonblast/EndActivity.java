739bcdd2c710cac301c9a84b516932d723fbd3f4
Mdsehab <last6882@gmail.com>
2019-07-12 12:06:21 +0600

    Welcome Main and End activity changed.

--- app/src/main/java/com/sehab/pranta/balloonblast/EndActivity.java
+++ app/src/main/java/com/sehab/pranta/balloonblast/EndActivity.java
@@ -2,6 +2,8 @@ package com.sehab.pranta.balloonblast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class EndActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class EndActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end);
-    }
+    } //on create ends
+
+
 }
--- app/src/main/java/com/sehab/pranta/balloonblast/WelcomeActivity.java
+++ app/src/main/java/com/sehab/pranta/balloonblast/WelcomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -21,6 +22,24 @@ public class WelcomeActivity extends AppCompatActivity {
                startActivity(intent);
             }
         });
+    } //onCreate ends
+
+     private void  FullScreen(){
+        ViewGroup rootLayout = (ViewGroup) findViewById(R.id.activity_welcome);
+        rootLayout.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FullScreen();
+    }
+
 
 }
